9ade1fde9d9324cbfb6ca717c93d5c58d40beb4b
Shane Holloway <shane.holloway@ieee.org>
2020-01-29 17:32:31 -0700

    Added mocha-based unittest template

---
+++ tests/mocha/finish-init.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# npm install --save-dev @rollup/plugin-commonjs
+
+npm install --save-dev \
+  rollup @rollup/plugin-node-resolve \
+  rollup-plugin-jsy-lite
+
+npm install --save-prod \
+  mocha chai
+
+npm -s run test
+
+echo "To use the parent package properly, install it relatively:"
+echo "  > npm install .. "
+echo
+
+echo "You may now remove '$0'"
+
