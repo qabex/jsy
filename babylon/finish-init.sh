7331dfbe51d7b688bc302df6330e105cb6245183
Shane Holloway <shane.holloway@ieee.org>
2020-04-20 14:28:32 -0600

    Changed to use babylon via script reference for speedy edit/run loop

--- babylon/finish-init.sh
+++ babylon/finish-init.sh
@@ -2,9 +2,7 @@
 
 npm install --save-dev \
   rollup @rollup/plugin-node-resolve \
-  rollup-plugin-jsy rollup-plugin-dgnotify \
-  qsrv \
-  @babylonjs/core
+  rollup-plugin-jsy rollup-plugin-dgnotify qsrv
 
 echo "You may now remove '$0'"
 
