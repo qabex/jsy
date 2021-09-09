0728d8e56090c023d39d53628ca2df1aa3cecf76
Shane Holloway <shane.holloway@ieee.org>
2021-09-09 13:26:45 -0600

    Updated jsy basic template

--- basic/rollup.config.js
+++ basic/rollup.config.js
@@ -6,20 +6,22 @@ import rpi_resolve from '@rollup/plugin-node-resolve'
 // import { terser as rpi_terser } from 'rollup-plugin-terser'
 
 
+const _rpis_ = (defines, ...args) => [
+  rpi_jsy({defines}),
+  rpi_resolve(),
+  ...args,
+  // rpi_commonjs(), // Allow CommonJS use -- https://github.com/rollup/plugins/tree/master/packages/commonjs#readme
+  rpi_dgnotify()]
+
+
 const _cfg_ = {
-  external: id => builtinModules.includes(id),
-  plugins: [
-    rpi_dgnotify(),
-    rpi_resolve(),
-    // rpi_commonjs(), // Allow CommonJS use -- https://github.com/rollup/plugins/tree/master/packages/commonjs#readme
+  external: id => /^node:/.test(id) || builtinModules.includes(id),
+  plugins: _rpis_({})
 
-    rpi_jsy({defines:{}}),
-  ]}
 
-/// Allow Minification -- https://github.com/TrySound/rollup-plugin-terser
-// import { terser as rpi_terser } from 'rollup-plugin-terser'
-// const _cfg_min_ = { ... _cfg_,
-//   plugins: [ ... _cfg_.plugins, rpi_terser() ]}
+// Allow Minification -- https://github.com/TrySound/rollup-plugin-terser
+const _cfg_min_ = 'undefined'===typeof rpi_terser ? null
+  : { ... _cfg_, plugins: [ ... _cfg_.plugins, rpi_terser() ]}
 
 
 export default [
@@ -30,17 +32,11 @@ export default [
 
 function * add_jsy(src_name, opt={}) {
   const input = `code/${src_name}${opt.ext || '.jsy'}`
-  //const module_name = opt.name || `${pkg_name}_${src_name}`
 
   yield { ..._cfg_, input,
-    output: [
-      { file: `esm/${src_name}.mjs`, format: 'es', sourcemap: true },
-      //{ file: `cjs/${src_name}.cjs`, format: 'cjs', exports: opt.exports || 'named', sourcemap: true },
-      //{ file: `umd/${src_name}.js`, format: 'umd', name:module_name, exports: opt.exports || 'named', sourcemap: true },
-    ]}
+    output: [{ file: `esm/${src_name}.mjs`, format: 'es', sourcemap: true }]}
 
-  if ('undefined' !== typeof _cfg_min_)
-    yield { ... _cfg_min_, input, output: [
-      //{ file: `umd/${src_name}.min.js`, format: 'umd', name:module_name, exports:opt.exports || 'named', sourcemap: false }
-      { file: `esm/${src_name}.min.mjs`, format: 'es', sourcemap: false } ]}
+  if (_cfg_min_)
+    yield { ... _cfg_min_, input,
+      output: [{ file: `esm/${src_name}.min.mjs`, format: 'es', sourcemap: false }]}
 }
