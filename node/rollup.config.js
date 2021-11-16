70db03e8750427d0391438bb91acac3674625ef3
Shane Holloway <shane.holloway@ieee.org>
2021-11-16 16:15:11 -0700

    Added template for "npm init jsy node"

---
+++ node/rollup.config.js
@@ -0,0 +1,40 @@
+import {builtinModules} from 'module'
+import rpi_jsy from 'rollup-plugin-jsy'
+import rpi_dgnotify from 'rollup-plugin-dgnotify'
+import rpi_resolve from '@rollup/plugin-node-resolve'
+
+import pkg from './package.json'
+
+pkg.dependencies ||= {} // ensure a dependencies dict
+
+const _rpis_ = (defines, ...args) => [
+  rpi_jsy({defines}),
+  rpi_resolve(),
+  ...args,
+  rpi_dgnotify()]
+
+
+const _cfg_ = {
+  external: id => (
+       /^\w+:/.test(id)
+    || builtinModules.includes(id)
+    || !! pkg.dependencies[id]
+    ),
+  plugins: _rpis_({}) }
+
+
+export default [
+  ... add_jsy('index'),
+]
+
+
+
+function * add_jsy(src_name, opt={}) {
+  const input = `code/${src_name}${opt.ext || '.jsy'}`
+
+  yield { ..._cfg_, input, output: [
+      { file: `esm/${src_name}.mjs`, format: 'es', sourcemap: true },
+      { file: `cjs/${src_name}.cjs`, format: 'cjs', sourcemap: true },
+    ].filter(Boolean)}
+
+}
